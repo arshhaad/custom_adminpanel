658af230f4075198fa75a99a5c492eb20d0b02fd
muhammed arshad <arshadkorlotty@gmail.com>
2025-12-26 16:12:49 +0530

    Fix dashboard URL by adding trailing slash

--- adminside/urls.py
+++ adminside/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path("login/", views.admin_login, name="admin_login"),
     path("logout/", views.admin_logout, name="admin_logout"),
-    path("dashboard", views.dashboard, name="dashboard"),
+    path("dashboard/", views.dashboard, name="dashboard"),
     # users...
     path("users/", views.user_list, name="user_list"),
     path("users/add/", views.user_add, name="user_add"),
